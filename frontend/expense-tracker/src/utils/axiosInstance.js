880ef845e06fb98d96bbff4b320d0b7f423aaf19
HarshitPatel3547 <harshitpatel3547@gmail.com>
2025-06-30 10:30:18 +0530

    Update axiosInstance.js

--- frontend/expense-tracker/src/utils/axiosInstance.js
+++ frontend/expense-tracker/src/utils/axiosInstance.js
@@ -1,3 +1,101 @@
+// import axios from "axios";
+// import { BASE_URL } from "./apiPaths";
+
+// const axiosInstance = axios.create({
+//   baseURL: BASE_URL,
+//   timeout: 10000,
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+// });
+
+// // Request Interceptor
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     const accessToken = localStorage.getItem("token");
+//     if (accessToken) {
+//       config.headers.Authorization = `Bearer ${accessToken}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+// // Response Interceptor
+// axiosInstance.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   (error) => {
+//     // Handle common errors globally
+//     if (error.response) {
+//       if (error.response.status === 401) {
+//         // Redirect to login page
+//         window.location.href = "/login";
+//       } else if (error.response.status === 500) {
+//         console.error("Server error. Please try again later.");
+//       }
+//     } else if (error.code === "ECONNABORTED") {
+//       console.error("Request timeout. Please try again.");
+//     }
+
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default axiosInstance;
+// import axios from "axios";
+// import { BASE_URL } from "./apiPaths";
+
+// const axiosInstance = axios.create({
+//   baseURL: BASE_URL,
+//   timeout: 10000,
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+// });
+
+// // Request Interceptor
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     const accessToken = localStorage.getItem("token");
+//     if (accessToken) {
+//       config.headers.Authorization = `Bearer ${accessToken}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+// // Response Interceptor
+// axiosInstance.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   (error) => {
+//     // Handle common errors globally
+//     if (error.response) {
+//       if (error.response.status === 401) {
+//         // Redirect to login page
+//         window.location.href = "/login";
+//       } else if (error.response.status === 500) {
+//         console.error("Server error. Please try again later.");
+//       }
+//     } else if (error.code === "ECONNABORTED") {
+//       console.error("Request timeout. Please try again.");
+//     }
+
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default axiosInstance;
+// utils/axiosInstance.js
+
 import axios from "axios";
 import { BASE_URL } from "./apiPaths";
 
@@ -19,20 +117,15 @@ axiosInstance.interceptors.request.use(
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
+
 // Response Interceptor
 axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
+  (response) => response,
   (error) => {
-    // Handle common errors globally
     if (error.response) {
       if (error.response.status === 401) {
-        // Redirect to login page
         window.location.href = "/login";
       } else if (error.response.status === 500) {
         console.error("Server error. Please try again later.");
